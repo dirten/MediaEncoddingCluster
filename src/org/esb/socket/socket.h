e7fb58859d4e49479ead1fb1f24b867b7a23f6d7
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2007-04-16 09:53:30 +0000

    - update
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@43 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ src/org/esb/socket/socket.h
@@ -0,0 +1,51 @@
+#ifndef SOCKET_H
+#define SOCKET_H
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+
+#if defined( __APPLE__ )
+#define SOCKET_NOSIGNAL SO_NOSIGPIPE
+#elif defined( unix ) && !defined( __CYGWIN__ ) && !defined( sun )
+#define SOCKET_NOSIGNAL MSG_NOSIGNAL
+#else
+#define SOCKET_NOSIGNAL 0
+#endif
+
+struct Socket_t{
+    char * hostname;
+    int port;
+};
+
+struct SocketData{
+    char *data;
+    int data_length;
+};
+
+class Socket{
+    private:
+	int 	port;
+	char *	hostname;
+	int 	socketFd;
+	int 	connectFd;
+	struct sockaddr_in socketaddr;
+	void 	init();
+    public:
+			Socket(int);
+			Socket();
+			Socket(char * hostname, int portnumber);
+			~Socket();
+	void 		write(const unsigned char*, int );
+	void 		write(SocketData* );
+	SocketData    *	read();
+	void 		setHostname(char*);
+	void 		setPort(int);
+	char 	      * getHostname();
+	int 		getPort();
+	void		Listen();
+	int		Accept();
+	void		Connect();
+	void		Close();
+};
+#endif
