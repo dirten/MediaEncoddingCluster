8dae93253d0c92fe7ad98348aed63be776db8904
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2007-04-19 23:03:43 +0000

    implementierung  hive controller
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@60 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ src/org/esb/hivecontrol/ClientHandler.cpp
@@ -0,0 +1,4 @@
+#include "org/esb/hivecontrol/ClientHandler.h"
+
+
+
---
+++ src/org/esb/hivecontrol/ClientHandler.h
@@ -0,0 +1,17 @@
+#include "org/esb/hivecontrol/ClientData.h"
+#include "org/esb/lang/Runnable.h"
+
+using namespace org::esb::lang;
+namespace org{
+    namespace esb{
+	namespace hive{
+	    class ClientHandler: public Runnable{
+		public:
+		    ClientHandler(ClientData *);
+		    void run();
+	    };
+	}
+    }
+}
+
+
