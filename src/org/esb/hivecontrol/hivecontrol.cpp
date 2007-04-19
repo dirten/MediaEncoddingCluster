9e142ac5ecb7abbc0cb9c5524804b5ac17bb569e
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2007-04-19 18:54:58 +0000

    Update Library
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@57 7add9c9f-332d-0410-adcc-f7b1da7091d4

--- src/org/esb/hivecontrol/hivecontrol.cpp
+++ src/org/esb/hivecontrol/hivecontrol.cpp
@@ -1,3 +1,11 @@
 #include "hivecontrol.h"
 
+using namespace org::esb::hive;
+using namespace org::esb::config;
+
+HiveControl::HiveControl(){
+    Config::init("cluster.cfg");
+
+}
+
 
--- src/org/esb/hivecontrol/hivecontrol.h
+++ src/org/esb/hivecontrol/hivecontrol.h
@@ -1,6 +1,6 @@
 #ifndef HIVECONTROL_H
 #define HIVECONTROL_H
-
+#include "org/esb/config/config.h"
 
 namespace org
 {
