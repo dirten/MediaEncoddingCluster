8a2a2b603ef79240e9ce2885824bd8b4c2553d98
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2007-05-10 17:26:48 +0000

    update Socket
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@83 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ src/listener/proto_unknown.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "proto_command.h"
+#include "org/esb/net/Socket.h"
+using namespace std;
+using namespace org::esb::net;
+
+
+class ProtoUnknown : public ProtoCommand{
+    private:
+	Socket * socket;
+    public:
+	ProtoUnknown(Socket * socket){
+	    this->socket=socket;
+	}
+	
+	~ProtoUnknown(){}
+	bool isResponsible(char * command){
+	    if(strlen(command)>0){
+		return true;
+	    }
+	    return true;
+	}
+	void process(char * command){
+	    string error="Unknown Command:";
+            error+=command;
+            error+="\n";
+            socket->getOutputStream()->write((char*)error.c_str(),error.length());
+//            delete error;
+	}
+};
