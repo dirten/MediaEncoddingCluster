948b199a55ecd279da333583b028f1ba99f1e575
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2007-05-12 20:47:41 +0000

    - disconnect als ProtoListener
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@90 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ src/listener/proto_disconnect.cpp
@@ -0,0 +1,28 @@
+#include "proto_command.h"
+
+
+
+class ProtoDisconnect : public ProtoCommand{
+    private:
+	Socket * socket;
+    public:
+	ProtoDisconnect(Socket * socket){
+	    this->socket=socket;
+	}
+	
+	~ProtoDisconnect(){}
+	bool isResponsible(char * command){
+	    if(strcmp(command,"disconnect")==0||
+	       strcmp(command,"quit")==0){
+		return true;
+	    }
+	    return false;
+	}
+	void process(char * command){
+	    string error="Disconnection:";
+            error+="\n";
+            socket->getOutputStream()->write((char*)error.c_str(),error.length());
+	    socket->close();
+//            delete error;
+	}
+};
