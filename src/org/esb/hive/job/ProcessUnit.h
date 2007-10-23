f3635364bb4c4330e79750d2783747c2f89f3c57
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2007-10-23 17:26:38 +0000

    - impl. ProcessUnit.cpp
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@192 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ src/org/esb/hive/job/ProcessUnit.h
@@ -0,0 +1,15 @@
+#ifndef ORG_ESB_HIVE_JOB_PROCESS_UNIT_H
+#define ORG_ESB_HIVE_JOB_PROCESS_UNIT_H
+
+namespace org{
+namespace esb{
+namespace hive{
+namespace job{
+class ProcessUnit{
+	
+
+
+};
+}}}}
+
+#endif
