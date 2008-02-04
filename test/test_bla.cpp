929393c9ce9769d03c41fea7a77147778bccff19
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2008-02-04 12:03:05 +0000

    - tests with objectoutputstream
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@216 7add9c9f-332d-0410-adcc-f7b1da7091d4

--- test/test_bla.cpp
+++ test/test_bla.cpp
@@ -1,6 +1,11 @@
 #include "org/esb/net/Socket.h"
+#include "org/esb/io/File.h"
 #include "org/esb/io/ObjectInputStream.h"
+#include "org/esb/io/ObjectOutputStream.h"
+#include "org/esb/io/FileOutputStream.h"
+#include "org/esb/io/FileInputStream.h"
 #include "org/esb/hive/job/ProcessUnit.h"
+
 using namespace std;
 using namespace org::esb::io;
 using namespace org::esb::net;
@@ -10,6 +15,27 @@ using namespace org::esb::hive::job;
 
 
 int main(){
+
+    File outfile("/tmp/test.file");
+    FileOutputStream fos(&outfile);
+    ObjectOutputStream oos(&fos);
+    
+    ProcessUnit unit_out;
+    Packet p1;
+    unit_out._input_packets.push_back(&p1);
+    oos.writeObject(unit_out);
+    fos.close();
+
+    File infile("/tmp/test.file");
+    FileInputStream fis(&infile);
+    ObjectInputStream ois(&fis);
+    
+    ProcessUnit unit_in;
+    ois.readObject(unit_in);
+
+
+
+/*
     Socket sock("localhost", 20000);
     sock.connect();
     ObjectInputStream ois(sock.getInputStream());
@@ -18,5 +44,7 @@ int main(){
     cout << "hier"<<endl;
     ProcessUnit unit;
     ois.readObject(unit);
+    
+    */
 }
 
