cab23311a33ccdf345f27bb207e6ad3770e26668
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2008-02-26 14:18:02 +0000

    - audio Decoder
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@234 7add9c9f-332d-0410-adcc-f7b1da7091d4

--- src/org/esb/av/Decoder.cpp
+++ src/org/esb/av/Decoder.cpp
@@ -8,7 +8,10 @@ Decoder::Decoder(): Codec(){}
 Decoder::Decoder(CodecID id): Codec(id,Codec::DECODER){}
 
 Frame Decoder::decode(Packet & packet){
-	return decodeVideo(packet);
+	if(this->codec_type==CODEC_TYPE_VIDEO)
+		return decodeVideo(packet);
+	if(this->codec_type==CODEC_TYPE_AUDIO)
+		return decodeAudio(packet);
 }
 
 Frame Decoder::decodeVideo(Packet & packet){
@@ -20,16 +23,16 @@ Frame Decoder::decodeVideo(Packet & packet){
     int bytesRemaining=packet.size,  bytesDecoded=0;
     uint8_t * rawData=packet.data;
     while(bytesRemaining > 0){
-	bytesDecoded=avcodec_decode_video(this, &frame, &_frameFinished, rawData, bytesRemaining);
-	if(bytesDecoded < 0){
-	    fprintf(stderr, "Error while decoding frame\n");
-	    break;
-	}
-	bytesRemaining-=bytesDecoded;
-	rawData+=bytesDecoded;
-	if(_frameFinished){
-	    break;
-	}
+		bytesDecoded=avcodec_decode_video(this, &frame, &_frameFinished, rawData, bytesRemaining);
+		if(bytesDecoded < 0){
+	    	fprintf(stderr, "Error while decoding frame\n");
+	    	break;
+		}
+		bytesRemaining-=bytesDecoded;
+		rawData+=bytesDecoded;
+		if(_frameFinished){
+	    	break;
+		}
     }
     frame._pixFormat=pix_fmt;
     frame.pts=packet.pts;
@@ -37,5 +40,21 @@ Frame Decoder::decodeVideo(Packet & packet){
     return frame;
 }
 Frame Decoder::decodeAudio(Packet & packet){
+		int size=packet.size, out_size;
+		uint8_t * outbuf = new uint8_t[AVCODEC_MAX_AUDIO_FRAME_SIZE];
+		uint8_t * inbuf=packet.data;
+        while (size > 0) {
+            int len = avcodec_decode_audio(this, (short *)outbuf, &out_size, inbuf, size);
+            if (len < 0) {
+                fprintf(stderr, "Error while decoding\n");
+                exit(1);
+            }
+            if (out_size > 0) {
+                /* if a frame has been decoded, output it */
+//                fwrite(outbuf, 1, out_size, outfile);
+            }
+            size -= len;
+            inbuf += len;
+        }
 
 }
