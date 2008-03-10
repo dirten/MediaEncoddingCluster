46dd6ac8c763ca1f77f2d4d3bec077a5bdefef0a
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2008-03-10 12:30:15 +0000

    - added WebServer
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@247 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ src/org/esb/web/WebServer.cpp
@@ -0,0 +1,56 @@
+#include "WebServer.h"
+#include <iostream>
+using namespace std;
+using namespace org::esb::web;
+
+bool WebServer::_toStop=false;
+static void show_index(struct shttpd_arg *arg){
+//	cout << "Request"<<endl;
+
+	shttpd_printf(arg, "%s",
+		"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
+		"<html><body><h1>Welcome to embedded example of SHTTPD");
+
+	shttpd_printf(arg, "<li>Upload file example. "
+	    "<form method=\"post\" enctype=\"multipart/form-data\" "
+	    "action=\"/post\"><input type=\"file\" name=\"file\">"
+	    "<input type=\"submit\"></form>");
+
+		arg->flags |= SHTTPD_END_OF_OUTPUT;
+
+}
+
+static void ssi_test(struct shttpd_arg *arg){
+//	cout << "test_ssi:"<<arg->user_data<<endl;
+}
+
+WebServer::WebServer(int port){
+//	struct shttpd_ctx	*ctx;
+	char c_port[10];
+	sprintf(c_port,"%d",port);
+	ctx = shttpd_init();
+	shttpd_set_option(ctx, "ports", c_port);
+	shttpd_set_option(ctx, "root", "src/org/esb/web");
+	shttpd_set_option(ctx, "access_log", "access.log");
+	shttpd_set_option(ctx, "error_log", "error.log");
+	shttpd_register_uri(ctx, "/", &show_index, (void *) NULL);
+
+	shttpd_register_ssi_func(ctx, "test", ssi_test, NULL);
+
+//	shttpd_register_uri(ctx, "/post", &show_post, NULL);
+
+	while (!WebServer::_toStop)
+		shttpd_poll(ctx, 1000);
+
+
+}
+
+void WebServer::run(){
+
+
+}
+void WebServer::stop(){
+	WebServer::_toStop=true;
+
+}
+
