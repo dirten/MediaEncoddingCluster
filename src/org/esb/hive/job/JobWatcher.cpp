164bf09ad5f73cc43ab5180b49f4fd9a37584f80
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2008-09-04 17:18:24 +0000

    - auskommentieren der log ausgabe
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@394 7add9c9f-332d-0410-adcc-f7b1da7091d4

--- src/org/esb/hive/job/JobWatcher.cpp
+++ src/org/esb/hive/job/JobWatcher.cpp
@@ -35,7 +35,7 @@ void JobWatcher::run(){
 	Connection con(Config::getProperty("db.connection"));
 //    Statement stmt_job=con.createStatement("update jobs set complete = now() where id=:id");
 	while(!_isStopSignal){
-	    logdebug("JobWatcher cycle");
+//	    logdebug("JobWatcher cycle");
         Statement stmt=con.createStatement("select jobs.id, job_details.id as detailid ,inputfile, outputfile, instream, outstream from jobs, job_details where jobs.id=job_details.job_id and complete is null order by jobs.id");
 	    ResultSet rs=stmt.executeQuery();
 	    while(rs.next()){
