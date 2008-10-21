77d0e21b5318a3bacb89bea9dcb2ab18e7f45238
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2008-10-21 08:54:26 +0000

    - sql interface build with MySQL Ver. 5.1
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@429 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ src/org/esb/sql/my_sql.h
@@ -0,0 +1,11 @@
+#ifndef ORG_ESB_SQL_MYSQL_H
+#define ORG_ESB_SQL_MYSQL_H
+#include <mysql_version.h>
+#ifdef WIN32 
+#if MYSQL_VERSION_ID < 50100
+#include <my_global.h>
+#endif
+#endif
+#include <mysql.h>
+
+#endif
