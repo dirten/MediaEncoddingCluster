71db89b6535c3b6b87b8b232c7ef1ad59b36e4b5
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2008-10-22 14:51:54 +0000

    -  added ScopedTimeCounter
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@433 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ src/org/esb/util/ScopedTimeCounter.h
@@ -0,0 +1,18 @@
+#ifndef ORG_ESB_UTIL_SCOPEDTIMECOUNTER_H
+#define ORG_ESB_UTIL_SCOPEDTIMECOUNTER_H
+#include <string>
+#include "boost/date_time/posix_time/posix_time.hpp"
+using namespace boost::posix_time;
+
+class ScopedTimeCounter
+{
+public:
+	ScopedTimeCounter(std::string name);
+	~ScopedTimeCounter(void);
+private:
+	ptime _start;
+	ptime _end;
+	std::string _name;
+};
+#endif
+
