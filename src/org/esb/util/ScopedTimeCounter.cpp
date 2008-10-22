b5bc5a975c8715831d0a59eab4b2c281df2a96eb
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2008-10-22 14:51:24 +0000

    -  added ScopedTimeCounter
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@432 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ src/org/esb/util/ScopedTimeCounter.cpp
@@ -0,0 +1,13 @@
+#include "ScopedTimeCounter.h"
+#include "Log.h"
+ScopedTimeCounter::ScopedTimeCounter(std::string name)
+{
+	_name=name;
+	_start=microsec_clock::local_time();
+}
+
+ScopedTimeCounter::~ScopedTimeCounter(void)
+{
+	_end=microsec_clock::local_time();
+	logdebug(_name<<" duration: "<<to_simple_string(_end-_start));
+}
