b8cc242b92b0f6c2763513c99a24057c475c5603
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2008-11-25 14:22:18 +0000

    - added new Container
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@465 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ src/org/esb/web/wtk/ElementContainer.h
@@ -0,0 +1,54 @@
+/* 
+ * File:   ElementContainer.h
+ * Author: jhoelscher
+ *
+ * Created on 25. November 2008, 10:22
+ */
+#include <map>
+#include <string>
+
+#include <Wt/Ext/Component>
+#include <Wt/WWebWidget>
+#include "Wt/WLabel"
+#include "Wt/Ext/LineEdit"
+#include "Wt/WContainerWidget"
+
+#ifndef _ELEMENTCONTAINER_H
+#define	_ELEMENTCONTAINER_H
+namespace org {
+  namespace esb {
+    namespace web {
+      namespace wtk {
+
+        template <typename T>
+        class ElementContainer {
+        public:
+          T * getElement(std::string name, std::string label, std::string value, Wt::WContainerWidget * parent);
+        private:
+          std::map<std::string, T*> _cont;
+        };
+
+        template <typename T>
+        T * ElementContainer<T>::getElement(std::string name, std::string label="", std::string value="", Wt::WContainerWidget * parent=0) {
+          if (_cont.find(name) == _cont.end()) {
+            Wt::WContainerWidget * c = new Wt::WContainerWidget(parent);
+            Wt::WLabel * elementLabel = new Wt::WLabel(label);
+            elementLabel->resize(Wt::WLength(14, Wt::WLength::FontEx), Wt::WLength());
+            T * element = new T();
+            c->addWidget(elementLabel);
+            c->addWidget(element);
+            element->setText(value);
+            element->resize(Wt::WLength(40, Wt::WLength::FontEx), Wt::WLength());
+            elementLabel->setBuddy(element);
+            _cont[name] = element;
+          }
+          return _cont[name];
+        }
+      }
+    }
+  }
+}
+
+
+#endif	/* _ELEMENTCONTAINER_H */
+
