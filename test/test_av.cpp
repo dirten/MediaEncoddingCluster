f09ec6060ffb1934708bcb7dc9ec5ee97b3be1d9
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2008-12-02 09:12:22 +0000

    - some compiler warnings from windows destroyed - some windows development - possible ffmpeg leak memory detected????
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@477 7add9c9f-332d-0410-adcc-f7b1da7091d4

--- test/test_av.cpp
+++ test/test_av.cpp
@@ -14,11 +14,12 @@ int main(int argc, char ** argv){
 	int loop=3;
 	while(loop-->0){
 	std::cout <<"loop "<<loop<<endl;
-	File f("/media/share/20070401 0140 - PREMIERE 3 - Ein Duke kommt selten allein (The Dukes of Hazzard).ts");
+//	File f("/media/share/20070401 0140 - PREMIERE 3 - Ein Duke kommt selten allein (The Dukes of Hazzard).ts");
+	File f("m:\\video\\20070401 0140 - PREMIERE 3 - Ein Duke kommt selten allein (The Dukes of Hazzard).ts");
 	FormatInputStream fis(&f);
 	PacketInputStream pis(&fis);
 	int a=0;
-	while(true||a<10000){
+	while(a<10000){
 		Packet p;
 		if(pis.readPacket(p)<0)break;
 		if(++a%1000==0){
