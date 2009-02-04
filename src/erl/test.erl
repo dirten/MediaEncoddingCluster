59e20fbccf12abe5ddfc05fa68ae682cb4393b9f
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2009-02-04 19:31:02 +0000

    - added erlang test
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@568 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ src/erl/test.erl
@@ -0,0 +1,47 @@
+-module(test).
+
+-export([insert/0, read/0, read2/0, create/0, drop/0,sequence/1]).
+-include("schema.hrl").
+-include("/usr/lib/erlang/lib/stdlib-1.15.1/include/qlc.hrl").
+%-include("stdlib/include/qlc.hrl").
+-record(sequence, {key, index}).
+
+create()->
+  mnesia:create_table(file,[{disc_copies, [node()]},{attributes, record_info(fields, file)}]),
+  mnesia:create_table(watchfolder,[{disc_copies, [node()]},{attributes, record_info(fields, watchfolder)}]),
+  mnesia:create_table(stream,[{disc_copies, [node()]},{attributes, record_info(fields, stream)}]),
+  mnesia:create_table(sequence, [{type, set}, {disc_copies,[node()]}, {attributes, record_info(fields, sequence)}]).
+
+drop()->
+  mnesia:delete_table(file),
+  mnesia:delete_table(watchfolder),
+  mnesia:delete_table(stream),
+  mnesia:delete_table(sequence).
+
+init_sequence(Name, Value) ->
+ 	  {atomic, ok} =mnesia:transaction(fun() ->mnesia:write(#sequence{key=Name, index=Value})end),
+ 	  ok.
+      
+sequence(Name, Inc) ->
+ 	     mnesia:dirty_update_counter(sequence, Name, Inc).
+
+sequence(Name) ->
+  sequence(Name, 1).
+
+
+insert()->
+  R=#file{id=2, filename=test.avi, path='/tmp', size=100, containertype=avi,streamcount=2,duration=12000,bitrate=512000,type=0,parent=0},
+  mnesia:dirty_write(R).
+
+read()->
+  File = #file{filename = '$1', path='$2',_='_'},
+  E=mnesia:dirty_select(file,[{File,[],['$2','$1']}]),
+  E.
+
+read2()->
+  F = fun() ->
+    Q = qlc:q([filename:join([E#file.path,E#file.filename]) || E <- mnesia:table(file)]),
+    qlc:e(Q)
+  end,
+  {atomic,E}=mnesia:transaction(F),
+  E.
