e65005046a0b486c07cb4c5a271bbe0cc6e8f5ce
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2009-02-08 12:19:32 +0000

    - this file is auto created
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@578 7add9c9f-332d-0410-adcc-f7b1da7091d4

--- src/erl/config.hrl
+++
@@ -1,3 +0,0 @@
--define(FILEPORTEXE,"M:/MediaEncodingCluster-build/src/erl/Debug/erltest" ).
-%-define(FILEPORTEXE,"/usr/bin/valgrind --log-file=/tmp/erltest  --tool=memcheck --leak-check=full --show-reachable=yes M:/MediaEncodingCluster-build/src/erl/Debug/erltest" ).
--define(ENCODECLIENTEXE,"M:/MediaEncodingCluster-build/src/erl/Debug/erlclient" ).
