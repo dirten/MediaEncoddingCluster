ac0a453d33a9f28b53a1bf923d32f2102b8a856a
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2009-05-20 13:24:03 +0000

    -release development
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@705 7add9c9f-332d-0410-adcc-f7b1da7091d4

--- src/erl/core/mhive_webserver.erl
+++ src/erl/core/mhive_webserver.erl
@@ -1,3 +1,28 @@
+%%%----------------------------------------------------------------------
+%%% File    : mhive_webserver.erl
+%%% Author  : Jan Hölscher <jan.hoelscher@esblab.com>
+%%% Purpose : Start and Stop the internal Webserver for the JSON API
+%%% Created : 18 Feb 2009 by Jan Hölscher <jan.hoelscher@esblab.com>
+%%%
+%%%
+%%% MediaEncodingCluster, Copyright (C) 2001-2009   Jan Hölscher
+%%%
+%%% This program is free software; you can redistribute it and/or
+%%% modify it under the terms of the GNU General Public License as
+%%% published by the Free Software Foundation; either version 2 of the
+%%% License, or (at your option) any later version.
+%%%
+%%% This program is distributed in the hope that it will be useful,
+%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%%% General Public License for more details.
+%%%
+%%% You should have received a copy of the GNU General Public License
+%%% along with this program; if not, write to the Free Software
+%%% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+%%% 02111-1307 USA
+%%%
+%%%----------------------------------------------------------------------
 -module(mhive_webserver).
 -export([start_link/0,terminate/2, init/1,code_change/3,handle_call/3,handle_cast/2,handle_info/2]).
 -behaviour(gen_server).
@@ -9,6 +34,7 @@ init([])->
   process_flag(trap_exit, true),
   inets:start(),
   _Old=[{file,config:get(wwwroot)++"/conf/8080.conf"}],
+  filelib:ensure_dir(getConfig(error_log)),
   Conf=[
     {port,getConfig(port)},
     {server_name,getConfig(server_name)},
