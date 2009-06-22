8b2d0082ccf5bd0f4fc25d3f5f5d5d43c68db4f4
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2009-06-22 09:12:59 +0000

    - depricated
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@749 7add9c9f-332d-0410-adcc-f7b1da7091d4

--- src/org/esb/av/FormatInputStream.h
+++ src/org/esb/av/FormatInputStream.h
@@ -12,47 +12,51 @@
 
 #include "AV.h"
 
-
+#ifdef WIN32
+#define DEPRICATED(func) __declspec(deprecated(desc)) func
+#else
+#define DEPRICATED(f) __attribute__((deprecated)) f
+#endif
 //extern "C" AVStream;
 using namespace org::esb::io;
 namespace org {
-  namespace esb {
-    namespace av {
-      //	    	logger("hive.av")
+    namespace esb {
+        namespace av {
+            //	    	logger("hive.av")
 
-      class FormatInputStream : public FormatBaseStream, public InputStream {
-        logger("hive.av.formatis");
+            class FormatInputStream : public FormatBaseStream, public InputStream {
+                logger("hive.av.formatis");
 
-      public:
-        FormatInputStream(File * source);
-        ~FormatInputStream();
-        __attribute__((deprecated)) AVFormatContext * getFormatContext();
-        long long int available(bool isBlocking = false);
-        long long int getFileSize();
-        bool isValid();
-        int read(unsigned char * buffer, int length);
-        int read(vector<unsigned char>&buffer);
-        int read();
-        int getStreamCount();
-        //                __attribute__((deprecated))InputStream * getStream(int sNumber);
-        __attribute__((deprecated)) InputStream * getStream(int sNumber);
-        __attribute__((deprecated)) AVInputStream * getAVStream(int sNumber);
-        void close();
-        void dumpFormat();
-        int seek(int stream_index, int64_t timestamp);
-        int selectedStream;
-        StreamInfo * getStreamInfo(int idx);
-      private:
-        org::esb::io::File * _sourceFile;
-        bool _isValid;
-        std::map<int, int> _streamMap;
-        std::map<int, int> _streamReverseMap;
-        friend class PacketInputStream;
-        AVFormatContext * formatCtx;
+            public:
+                FormatInputStream(File * source);
+                ~FormatInputStream();
+                DEPRICATED(AVFormatContext * getFormatContext());
+                long long int available(bool isBlocking = false);
+                long long int getFileSize();
+                bool isValid();
+                int read(unsigned char * buffer, int length);
+                int read(vector<unsigned char>&buffer);
+                int read();
+                int getStreamCount();
+                //                __attribute__((deprecated))InputStream * getStream(int sNumber);
+               DEPRICATED(InputStream * getStream(int sNumber));
+               DEPRICATED(AVInputStream * getAVStream(int sNumber));
+                void close();
+                void dumpFormat();
+                int seek(int stream_index, int64_t timestamp);
+                int selectedStream;
+                StreamInfo * getStreamInfo(int idx);
+            private:
+                org::esb::io::File * _sourceFile;
+                bool _isValid;
+                std::map<int, int> _streamMap;
+                std::map<int, int> _streamReverseMap;
+                friend class PacketInputStream;
+                AVFormatContext * formatCtx;
 
-      };
+            };
+        }
     }
-  }
 }
 
 #endif
