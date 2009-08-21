be1b864148a9c188510be92cd07db8402117de9a
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2009-08-21 23:49:29 +0000

    - windows service
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@875 7add9c9f-332d-0410-adcc-f7b1da7091d4

--- src/Hive.cpp
+++ src/Hive.cpp
@@ -398,9 +398,55 @@ VOID WINAPI SvcMain( DWORD dwArgc, LPTSTR *lpszArgv )
         SvcReportEvent(TEXT("RegisterServiceCtrlHandler")); 
         return; 
     } 
+gSvcStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS; 
+    gSvcStatus.dwServiceSpecificExitCode = 0;    
+
+    // Report initial status to the SCM
+
+    ReportSvcStatus( SERVICE_START_PENDING, NO_ERROR, 3000 );
+	SvcInit( dwArgc, lpszArgv );
 
 
 }
+VOID SvcInit( DWORD dwArgc, LPTSTR *lpszArgv)
+{
+    // TO_DO: Declare and set any required variables.
+    //   Be sure to periodically call ReportSvcStatus() with 
+    //   SERVICE_START_PENDING. If initialization fails, call
+    //   ReportSvcStatus with SERVICE_STOPPED.
+
+    // Create an event. The control handler function, SvcCtrlHandler,
+    // signals this event when it receives the stop control code.
+
+    ghSvcStopEvent = CreateEvent(
+                         NULL,    // default security attributes
+                         TRUE,    // manual reset event
+                         FALSE,   // not signaled
+                         NULL);   // no name
+
+    if ( ghSvcStopEvent == NULL)
+    {
+        ReportSvcStatus( SERVICE_STOPPED, NO_ERROR, 0 );
+        return;
+    }
+
+    // Report running status when initialization is complete.
+
+    ReportSvcStatus( SERVICE_RUNNING, NO_ERROR, 0 );
+
+    // TO_DO: Perform work until service stops.
+
+    while(1)
+    {
+        // Check whether to stop the service.
+
+        WaitForSingleObject(ghSvcStopEvent, INFINITE);
+
+        ReportSvcStatus( SERVICE_STOPPED, NO_ERROR, 0 );
+        return;
+    }
+}
+
 void start_win32(){
 SERVICE_TABLE_ENTRY DispatchTable[] = 
     { 
