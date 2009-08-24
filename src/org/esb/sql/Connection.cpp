9a5b06969174f820fe78ac849fb1626419616bd1
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2009-08-24 08:02:53 +0000

    - enable debug for wt
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@886 7add9c9f-332d-0410-adcc-f7b1da7091d4

--- src/org/esb/sql/Connection.cpp
+++ src/org/esb/sql/Connection.cpp
@@ -76,7 +76,8 @@ void Connection::connect() {
   }
 
   mysqlPtr = boost::shared_ptr<MYSQL > (mysql_init(NULL), &mysql_close);
-  mysql_options(mysqlPtr.get(), MYSQL_OPT_USE_EMBEDDED_CONNECTION,NULL);
+//  mysql_options(mysqlPtr.get(), MYSQL_OPT_USE_EMBEDDED_CONNECTION,NULL);
+  mysql_options(mysqlPtr.get(), MYSQL_OPT_USE_REMOTE_CONNECTION,NULL);
   if (!mysql_real_connect(mysqlPtr.get(), _host.c_str(), _username.c_str(), _passwd.c_str(), _db.c_str(), 0, NULL, 0)) {
     throw SqlException(string("Failed to connect to database: ").append(std::string(mysql_error(mysqlPtr.get()))));
   }
