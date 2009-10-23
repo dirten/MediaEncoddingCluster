3571ce8b3277c2c6837043734b68c3da2d1271de
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2009-10-23 16:25:29 +0000

    - database as a service for controled startup and shutdown
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@989 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ src/org/esb/hive/DatabaseService.cpp
@@ -0,0 +1,75 @@
+/*----------------------------------------------------------------------
+ *  File    : DatabaseService.cpp
+ *  Author  : Jan Hölscher <jan.hoelscher@esblab.com>
+ *  Purpose : Global Database Service for the Database Connection
+ *  Created : 18 Feb 2009 by Jan Hölscher <jan.hoelscher@esblab.com>
+ *
+ *
+ * MediaEncodingCluster, Copyright (C) 2001-2009   Jan Hölscher
+ *
+ * This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation; either version 2 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307 USA
+ *
+ * ----------------------------------------------------------------------
+ */
+#include "DatabaseService.h"
+#include "org/esb/signal/Messenger.h"
+#include "org/esb/sql/my_sql.h"
+namespace org {
+  namespace esb {
+    namespace hive {
+
+      DatabaseService::DatabaseService(std::string base_path):_base_path(base_path) {
+        _running = false;
+      }
+
+      void DatabaseService::onMessage(org::esb::signal::Message & msg) {
+       if (msg.getProperty("databaseservice") == org::esb::hive::START) {
+         start();
+       }
+       if (msg.getProperty("databaseservice") == org::esb::hive::STOP) {
+         stop();
+       }
+      }
+
+      void DatabaseService::start() {
+        if (_running == false) {
+          std::string base_path = _base_path;
+
+          std::string lang = "--language=";
+          lang.append(base_path);
+          lang.append("/res");
+
+          std::string datadir = "--datadir=";
+          datadir.append(base_path);
+          datadir.append("/");
+          static char *server_options[] = {"mysql_test", const_cast<char*> (datadir.c_str()), const_cast<char*> (lang.c_str()), NULL};
+          int num_elements = (sizeof (server_options) / sizeof (char *)) - 1;
+          static char *server_groups[] = {"embedded", NULL};
+          if (mysql_library_init(num_elements, server_options, server_groups) != 0) {
+            //      logerror("DB Library init failed");
+          }
+          _running = true;
+        }
+      }
+
+      void DatabaseService::stop() {
+        if(_running)
+           mysql_library_end();
+        _running=false;
+      }
+    }
+  }
+}
---
+++ src/org/esb/hive/DatabaseService.h
@@ -0,0 +1,54 @@
+/*----------------------------------------------------------------------
+ *  File    : DatabaseService.h
+ *  Author  : Jan Hölscher <jan.hoelscher@esblab.com>
+ *  Purpose : Global Database Service for the Database Connection
+ *  Created : 18 Feb 2009 by Jan Hölscher <jan.hoelscher@esblab.com>
+ *
+ *
+ * MediaEncodingCluster, Copyright (C) 2001-2009   Jan Hölscher
+ *
+ * This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation; either version 2 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307 USA
+ *
+ * ----------------------------------------------------------------------
+ */
+
+#ifndef _DATABASESERVICE_H
+#define	_DATABASESERVICE_H
+
+#include "org/esb/signal/MessageListener.h"
+#include "org/esb/signal/Message.h"
+#include <string>
+namespace org {
+    namespace esb {
+        namespace hive {
+
+            class DatabaseService : public org::esb::signal::MessageListener {
+            public:
+                DatabaseService(std::string);
+                void onMessage(org::esb::signal::Message & msg);
+            private:
+                void start();
+                void stop();
+                bool _running;
+                std::string _base_path;
+            };
+        }
+    }
+}
+
+
+#endif	/* _DATABASESERVICE_H */
+
