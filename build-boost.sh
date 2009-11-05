6ffebecbdf3ebf32e5e73456c8b7cc9b0b3c2354
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2009-11-05 12:01:20 +0000

    - test commit
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@1023 7add9c9f-332d-0410-adcc-f7b1da7091d4

--- build-boost.sh
+++ build-boost.sh
@@ -24,7 +24,7 @@ esac
 
 download_file "http://dfn.dl.sourceforge.net/project/boost/boost/1.36.0/boost_1_36_0.tar.bz2" "boost.src.tar.bz2" "boost.src.tar"
 bunzip_file "boost.src.tar.bz2" "boost.src.tar"
-untar_file "boost.src.tar" "boost_*" "boost"
+untar_file "boost.src.tar" "boost_1_36*" "boost"
 if test ! -f source/boost/tools/jam/stage/bin.${JAMBIN}x86/bjam ; then
   echo "Build Jam"
   cd source/boost/tools/jam
