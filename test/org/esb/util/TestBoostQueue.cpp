c5a298fac0dd130fdae000179775f7e3f75c0683
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2009-12-18 16:50:37 +0000

    - new test class added
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@1078 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ test/org/esb/util/TestBoostQueue.cpp
@@ -0,0 +1,86 @@
+/* 
+ * File:   TestBoostQueue.cpp
+ * Author: jhoelscher
+ *
+ * Created on 17. Dezember 2009, 11:52
+ */
+
+#include <stdlib.h>
+#include <boost/interprocess/ipc/message_queue.hpp>
+#include <boost/thread.hpp>
+#include <boost/bind.hpp>
+#include "org/esb/lang/Thread.h"
+#include "org/esb/util/Log.h"
+/*
+ * 
+ */
+
+using namespace boost::interprocess;
+using namespace org::esb::lang;
+class queue_sender {
+public:
+
+  queue_sender() {
+    mq = new message_queue
+        (open_or_create //open or create
+        , "message_queue" //name
+        , 100 //max message number
+        , 100 //max message size
+        );
+  }
+
+  void send() {
+    for (int a = 4; a < 10; a++) {
+      mq->send(&a, sizeof(a), 0);
+    }
+  }
+
+  ~queue_sender() {
+    message_queue::remove("message_queue");
+
+    delete mq;
+  }
+
+private:
+  message_queue * mq;
+};
+
+class queue_receiver {
+public:
+
+  queue_receiver() {
+    mq = new message_queue
+        (open_only //only open
+        , "message_queue" //name
+        );
+  }
+  void receive(){
+    int number;
+    unsigned int prio;
+    std::size_t recvd_size;
+    try{
+    mq->receive(&number, sizeof(number),recvd_size,prio);
+    }catch(interprocess_exception &ex){
+      logerror(ex.what());
+    }
+
+    logdebug("Received number:"<<number);
+  }
+  ~queue_receiver() {
+    delete mq;
+  }
+private:
+  message_queue * mq;
+
+};
+
+int main(int argc, char** argv) {
+  queue_sender s;
+  queue_receiver r;
+  boost::thread sender_th(boost::bind(&queue_sender::send, &s));
+  Thread::sleep2(1000);
+  r.receive();
+  Thread::sleep2(10000);
+  return (EXIT_SUCCESS);
+}
+
