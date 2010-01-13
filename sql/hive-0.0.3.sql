6d00acbb850e92c24ac6d9232951d376052d3178
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2010-01-13 20:56:49 +0000

    - database changes
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@1107 7add9c9f-332d-0410-adcc-f7b1da7091d4

--- sql/hive-0.0.3.sql
+++ sql/hive-0.0.3.sql
@@ -7,94 +7,140 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+--
+-- Datenbank: `hive2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `codec`
+--
 
 CREATE TABLE IF NOT EXISTS `codec` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `codec_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `long_name` varchar(255) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `long_name` varchar(255) NOT NULL,
+  `extra` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `config`
+--
 
 CREATE TABLE IF NOT EXISTS `config` (
   `config_key` varchar(255) NOT NULL,
   `config_val` varchar(255) NOT NULL,
-  PRIMARY KEY  (`config_key`)
+  PRIMARY KEY (`config_key`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `files`
+--
 
 CREATE TABLE IF NOT EXISTS `files` (
-  `id` int(11) NOT NULL auto_increment,
-  `filename` varchar(255) default NULL,
-  `path` varchar(255) default NULL,
-  `size` bigint NOT NULL default '0',
-  `stream_count` int(2) NOT NULL default '0',
-  `container_type` varchar(255) default NULL,
-  `title` varchar(255) default NULL  ,
-  `author` varchar(255) default NULL,
-  `copyright` varchar(255) default NULL,
-  `comment` varchar(255) default NULL,
-  `album` varchar(255) default NULL,
-  `year` int(4) NOT NULL default '0',
-  `track` int(1) NOT NULL default '0',
-  `genre` varchar(255) default NULL,
-  `duration` bigint NOT NULL default '0',
-  `bitrate` int(11) NOT NULL default '0',
-  `insertdate` timestamp NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  `type` int(1) NOT NULL default '0',
-  `parent` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `filename` varchar(255) DEFAULT NULL,
+  `path` varchar(255) NOT NULL,
+  `size` int(11) NOT NULL DEFAULT '0',
+  `stream_count` int(2) NOT NULL DEFAULT '0',
+  `container_type` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `copyright` varchar(255) NOT NULL,
+  `comment` varchar(255) NOT NULL,
+  `album` varchar(255) NOT NULL,
+  `year` int(4) NOT NULL DEFAULT '0',
+  `track` int(1) NOT NULL DEFAULT '0',
+  `genre` varchar(255) NOT NULL,
+  `duration` bigint(20) NOT NULL DEFAULT '0',
+  `bitrate` int(11) NOT NULL DEFAULT '0',
+  `insertdate` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `type` int(1) NOT NULL DEFAULT '0',
+  `parent` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `frame_groups`
+--
 
 CREATE TABLE IF NOT EXISTS `frame_groups` (
-  `id` int(11) NOT NULL auto_increment,
-  `jobid` int(11) NOT NULL default '0',
-  `frame_group` int(11) default NULL,
-  `startts` double NOT NULL default '0',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `jobid` int(11) NOT NULL DEFAULT '0',
+  `frame_group` int(11) DEFAULT NULL,
+  `startts` double NOT NULL DEFAULT '0',
   `byte_pos` double NOT NULL,
-  `stream_id` int(11) default NULL,
-  `stream_index` int(11) NOT NULL default '0',
-  `frame_count` bigint NOT NULL default '0',
-  `sended` timestamp NULL default NULL,
-  `complete` timestamp NULL default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-
-CREATE TABLE IF NOT EXISTS `jobs` (
-  `id` int(11) NOT NULL auto_increment,
-  `inputfile` int(11) default NULL,
-  `outputfile` int(11) default NULL,
-  `begin` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  `complete` timestamp NULL default NULL,
-  `profileid` int(11) default NULL,
-  `status` varchar(255) default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-
-CREATE TABLE IF NOT EXISTS `job_details` (
-  `id` int(11) NOT NULL auto_increment,
-  `job_id` int(11) default NULL,
-  `instream` int(11) default NULL,
-  `outstream` int(11) default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-
-CREATE TABLE IF NOT EXISTS `job_logs` (
-  `id` int(11) NOT NULL auto_increment,
-  `packet_in` int(11) default NULL,
-  `packet_out` int(11) default NULL,
-  `begin` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  `complete` timestamp NOT NULL default '0000-00-00 00:00:00',
-  PRIMARY KEY  (`id`)
+  `stream_id` int(11) DEFAULT NULL,
+  `stream_index` int(11) NOT NULL DEFAULT '0',
+  `frame_count` int(11) NOT NULL DEFAULT '0',
+  `sended` timestamp NULL DEFAULT NULL,
+  `complete` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `jobs`
+--
+
+CREATE TABLE IF NOT EXISTS `jobs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `inputfile` int(11) DEFAULT NULL,
+  `outputfile` int(11) DEFAULT NULL,
+  `begin` timestamp NULL DEFAULT NULL,
+  `complete` timestamp NULL DEFAULT NULL,
+  `profileid` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `job_details`
+--
+
+CREATE TABLE IF NOT EXISTS `job_details` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `job_id` int(11) DEFAULT NULL,
+  `instream` int(11) DEFAULT NULL,
+  `outstream` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `job_logs`
+--
+
+CREATE TABLE IF NOT EXISTS `job_logs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `packet_in` int(11) DEFAULT NULL,
+  `packet_out` int(11) DEFAULT NULL,
+  `begin` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `complete` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `packets`
+--
 
 CREATE TABLE IF NOT EXISTS `packets` (
-`id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `stream_id` int(11) DEFAULT NULL,
   `pts` bigint(20) DEFAULT NULL,
   `dts` bigint(20) DEFAULT NULL,
@@ -103,90 +149,120 @@ CREATE TABLE IF NOT EXISTS `packets` (
   `frame_group` int(11) DEFAULT NULL,
   `flags` int(11) DEFAULT NULL,
   `duration` int(11) DEFAULT NULL,
-  `pos` bigint(20) DEFAULT NULL,
-  `sort` bigint(20) NOT NULL DEFAULT '0',
+  `pos` int(11) DEFAULT NULL,
+  `sort` bigint(20) NOT NULL,
   `data_size` int(11) DEFAULT NULL,
   `data` mediumblob,
   PRIMARY KEY (`id`),
   KEY `packet_group_idx` (`frame_group`),
   KEY `pts` (`pts`),
   KEY `dts` (`dts`),
-  KEY `stream_id` (`stream_id`),
-  KEY `sid_dts_idx` (`stream_id`,`dts`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  KEY `stream_id_2` (`stream_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `process_units`
+--
 
 CREATE TABLE IF NOT EXISTS `process_units` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `source_stream` int(11) NOT NULL,
   `target_stream` int(11) NOT NULL,
   `start_ts` bigint(20) NOT NULL,
   `end_ts` bigint(20) NOT NULL,
-  `frame_count` bigint NOT NULL,
-  `send` timestamp NULL default NULL,
-  `complete` timestamp NULL default NULL,
-  `priority` tinyint(1) NOT NULL default '5',
-  PRIMARY KEY  (`id`),
-  KEY `target_stream` (`target_stream`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `frame_count` int(11) NOT NULL,
+  `send` timestamp NULL DEFAULT NULL,
+  `complete` timestamp NULL DEFAULT NULL,
+  `priority` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=111 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `profiles`
+--
 
 CREATE TABLE IF NOT EXISTS `profiles` (
-  `id` int(11) NOT NULL auto_increment,
-  `profile_name` varchar(255) default NULL,
-  `v_format` varchar(255) default NULL,
-  `v_codec` int(11) default NULL,
-  `v_bitrate` int(11) default NULL,
-  `v_framerate` int(11) default NULL,
-  `v_width` int(11) default NULL,
-  `v_height` int(11) default NULL,
-  `a_channels` int(11) default NULL,
-  `a_codec` int(11) default NULL,
-  `a_bitrate` int(11) default NULL,
-  `a_samplerate` int(11) default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `profile_name` varchar(255) DEFAULT NULL,
+  `v_format` varchar(255) DEFAULT NULL,
+  `v_format_ext` varchar(10) DEFAULT NULL,
+  `v_codec` int(11) DEFAULT NULL,
+  `v_bitrate` int(11) DEFAULT NULL,
+  `v_framerate` varchar(20) DEFAULT NULL,
+  `v_width` int(11) DEFAULT NULL,
+  `v_height` int(11) DEFAULT NULL,
+  `v_extra` varchar(1000) DEFAULT NULL,
+  `a_channels` int(11) DEFAULT NULL,
+  `a_codec` int(11) DEFAULT NULL,
+  `a_bitrate` int(11) DEFAULT NULL,
+  `a_samplerate` int(11) DEFAULT NULL,
+  `a_extra` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `statistics_log`
+--
 
 CREATE TABLE IF NOT EXISTS `statistics_log` (
-  `id` bigint(20) NOT NULL auto_increment,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `stat_key` varchar(255) NOT NULL,
   `stat_val` varchar(255) NOT NULL,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `streams`
+--
 
 CREATE TABLE IF NOT EXISTS `streams` (
-  `id` int(11) NOT NULL auto_increment,
-  `fileid` int(11) NOT NULL default '0',
-  `stream_index` int(11) NOT NULL default '0',
-  `stream_type` int(11) NOT NULL default '0',
-  `codec` int(11) NOT NULL default '0',
-  `codec_name` varchar(255) default NULL,
-  `framerate_num` int(11) NOT NULL default '0',
-  `framerate_den` int(11) NOT NULL default '0',
-  `start_time` bigint NOT NULL default '0',
-  `duration` bigint NOT NULL default '0',
-  `nb_frames` bigint NOT NULL default '0',
-  `time_base_num` int(11) NOT NULL default '0',
-  `time_base_den` int(11) NOT NULL default '0',
-  `framecount` int(11) NOT NULL default '0',
-  `width` int(11) NOT NULL default '0',
-  `height` int(11) NOT NULL default '0',
-  `gop_size` int(11) NOT NULL default '0',
-  `pix_fmt` int(11) NOT NULL default '0',
-  `bit_rate` int(11) NOT NULL default '0',
-  `rate_emu` int(11) NOT NULL default '0',
-  `sample_rate` int(11) NOT NULL default '0',
-  `channels` int(11) NOT NULL default '0',
-  `sample_fmt` int(11) NOT NULL default '0',
-  `flags` int(11) NOT NULL default '0',
-  `bits_per_coded_sample` int(11) NOT NULL default '0',
-  `priv_data_size` int(11) default '0',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fileid` int(11) NOT NULL DEFAULT '0',
+  `stream_index` int(11) NOT NULL DEFAULT '0',
+  `stream_type` int(11) NOT NULL DEFAULT '0',
+  `codec` int(11) NOT NULL DEFAULT '0',
+  `codec_name` varchar(255) DEFAULT NULL,
+  `framerate_num` int(11) NOT NULL DEFAULT '0',
+  `framerate_den` int(11) NOT NULL,
+  `start_time` bigint(20) NOT NULL DEFAULT '0',
+  `duration` bigint(20) NOT NULL DEFAULT '0',
+  `nb_frames` bigint(20) NOT NULL,
+  `time_base_num` int(11) NOT NULL DEFAULT '0',
+  `time_base_den` int(11) NOT NULL DEFAULT '0',
+  `framecount` int(11) NOT NULL DEFAULT '0',
+  `width` int(11) NOT NULL DEFAULT '0',
+  `height` int(11) NOT NULL DEFAULT '0',
+  `gop_size` int(11) NOT NULL DEFAULT '0',
+  `pix_fmt` int(11) NOT NULL DEFAULT '0',
+  `bit_rate` int(11) NOT NULL DEFAULT '0',
+  `rate_emu` int(11) NOT NULL DEFAULT '0',
+  `sample_rate` int(11) NOT NULL DEFAULT '0',
+  `channels` int(11) NOT NULL DEFAULT '0',
+  `sample_fmt` int(11) NOT NULL DEFAULT '0',
+  `bits_per_coded_sample` int(11) NOT NULL,
+  `priv_data_size` int(11) DEFAULT '0',
   `priv_data` blob,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `flags` int(10) unsigned NOT NULL,
+  `extra_profile_flags` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `user`
+--
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) unsigned NOT NULL auto_increment,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `auth_name` varchar(255) NOT NULL,
   `auth_passwd` varchar(255) NOT NULL,
   `first_name` varchar(255) NOT NULL,
@@ -195,26 +271,36 @@ CREATE TABLE IF NOT EXISTS `user` (
   `user_type` int(11) unsigned NOT NULL,
   `created` datetime NOT NULL,
   `updated` datetime NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `version`
+--
 
 CREATE TABLE IF NOT EXISTS `version` (
-  `id` int(11) NOT NULL auto_increment,
-  `component` varchar(255) default NULL,
-  `version` varchar(255) default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `component` varchar(255) DEFAULT NULL,
+  `version` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `watch_folder`
+--
 
 CREATE TABLE IF NOT EXISTS `watch_folder` (
-  `id` int(11) unsigned NOT NULL auto_increment,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `infolder` varchar(255) NOT NULL,
+  `profile` int(11) unsigned NOT NULL,
   `outfolder` varchar(255) NOT NULL,
   `extension_filter` varchar(255) NOT NULL,
-  `profile` int(11) unsigned NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
 
 
 INSERT INTO `config` VALUES('framerates', '1/10#10,1/15#15,1000/23976#23.976,1/24#24,1/25#25,100/2997#29.97,1/30#30');
