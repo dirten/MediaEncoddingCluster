a3ebe16f9089c24f0fddf4390e8c3fcd7f3250d9
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2010-02-02 00:03:56 +0000

    - something is going wrong in framerate conversion
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@1132 7add9c9f-332d-0410-adcc-f7b1da7091d4

--- src/org/esb/av/FrameConverter.cpp
+++ src/org/esb/av/FrameConverter.cpp
@@ -105,8 +105,8 @@ namespace org {
       void FrameConverter::compensateFrameRateConversion(Frame & input, Frame & out) {
         int frames = 1;
         if (_gop_size > 0||_gop_size==-1) {
-//          double a = static_cast<double> ((double) _dec->getLastTimeStamp() / (double) _dec->getTimeBase().den);
-          double a = static_cast<double> ((double) input.getPts() / (double) _dec->getTimeBase().den);
+          double a = static_cast<double> ((double) _dec->getLastTimeStamp() / (double) _dec->getTimeBase().den);
+//          double a = static_cast<double> ((double) input.getPts() / (double) _dec->getTimeBase().den);
           double delta = _frameRateCompensateBase + a / av_q2d(_enc->getTimeBase()) - _enc->getLastTimeStamp();
           if (delta >= 2.0)
             frames = static_cast<int> (floor(delta + 0.5));
