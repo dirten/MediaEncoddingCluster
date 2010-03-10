2c26458c5f947cba30143400fe4a34193df2318b
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2010-03-10 19:38:47 +0000

    - update sql schema 0.0.5
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@1192 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ sql/hive-0.0.5.sql
@@ -0,0 +1,9 @@
+alter IGNORE table profiles add v_deinterlace varchar(255) NOT NULL DEFAULT '0' AFTER v_extra;
+alter IGNORE table profiles add v_keep_aspect_ratio varchar(255) NOT NULL DEFAULT '0' AFTER v_deinterlace;
+alter IGNORE table streams add deinterlaced int(4) NOT NULL DEFAULT '0' AFTER height;
+alter IGNORE table jobs add deinterlace int(4) NOT NULL DEFAULT '0' AFTER outputfile;
+alter IGNORE table jobs add keep_display_aspect int(4) NOT NULL DEFAULT '0' AFTER deinterlace;
+alter table version drop id;
+alter table version add primary key cmp_idx (component);
+REPLACE INTO version (component,version) VALUES ('database.model','0.0.5');
+
