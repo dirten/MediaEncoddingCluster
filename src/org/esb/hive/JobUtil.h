b275a707fddcb709f6b1852803dbb22792c0163f
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2010-07-20 09:34:49 +0000

    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@1543 7add9c9f-332d-0410-adcc-f7b1da7091d4

--- src/org/esb/hive/JobUtil.h
+++ src/org/esb/hive/JobUtil.h
@@ -1,7 +1,7 @@
 #ifndef ORG_ESB_HIVE_JOBUTIL_H
 #define ORG_ESB_HIVE_JOBUTIL_H
 
-#include "org/esb/db/hivedb.hpp"
+
 int jobcreator(int fileid, int profileid, std::string outpath);
 int jobcreator(int argc, char*argv[]);
 
