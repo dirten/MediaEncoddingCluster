13e722b05524b4dbe4a55cb3d589d0d13ad17f34
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2010-07-25 15:20:28 +0000

    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@1595 7add9c9f-332d-0410-adcc-f7b1da7091d4

--- src/org/esb/web/DbTableModel.h
+++ src/org/esb/web/DbTableModel.h
@@ -5,35 +5,36 @@
 #include <Wt/WContainerWidget>
 #include <vector>
 #include "org/esb/util/Log.h"
+#include "org/esb/config/config.h"
 namespace org{
-namespace esb{
-namespace web{
-template <typename T>
-  class DbTableModel:public Wt::WStandardItemModel{
-  public:
-    DbTableModel(Wt::WContainerWidget* parent=0): 
-        Wt::WStandardItemModel(0, 0, parent),
-        _dbCon("mysql",Config::getProperty("db.url")){
-          std::vector<T> data=litesql::select<T>(_dbCon).all();
-          std::vector<litesql::FieldType> fields;
-          T tmp_model(_dbCon);
-          tmp_model.getFieldTypes(fields);
-          insertColumns(0, fields.size());
-          std::vector<litesql::FieldType>::iterator fieldit=fields.begin();
-          for(int a = 0;fieldit!=fields.end();fieldit++, a++){
-            LOGDEBUG("setting headername:"<<(*fieldit).name());
-            if (!setHeaderData(a, (*fieldit).name())) {
-            std::cout << "Failed" << std::endl;
-          }
-            
-          }
-    }
-    void clear();
-    void load();
-  private:
-    db::HiveDb _dbCon;
-};
-}}}
+  namespace esb{
+    namespace web{
+      template <typename T>
+      class DbTableModel:public Wt::WStandardItemModel{
+      public:
+        DbTableModel(Wt::WContainerWidget* parent=0): 
+            Wt::WStandardItemModel(0, 0, parent), _dbCon("mysql",org::esb::config::Config::getProperty("db.url")){
+              std::vector<T> data=litesql::select<T>(_dbCon).all();
+              std::vector<litesql::FieldType> fields;
+              T tmp_model(_dbCon);
+              tmp_model.getFieldTypes(fields);
+              
+              insertColumns(0, fields.size());
+              std::vector<litesql::FieldType>::iterator fieldit=fields.begin();
+              for(int a = 0;fieldit!=fields.end();fieldit++, a++){
+                LOGDEBUG("setting headername:"<<(*fieldit).name());
+                
+                if (!setHeaderData(a, (*fieldit).name())) {
+                  std::cout << "Failed" << std::endl;
+                }
+              }
+            }
+            void clear();
+            void load();
+      private:
+        db::HiveDb _dbCon;
+      };
+    }}}
 #endif
 
 
