b5dfc2a132e67bce4cf1977ca4d628221a7cb6a9
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2010-08-23 12:10:05 +0000

    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@1728 7add9c9f-332d-0410-adcc-f7b1da7091d4

--- src/org/esb/lang/StackDumper.cpp
+++ src/org/esb/lang/StackDumper.cpp
@@ -55,8 +55,8 @@ bool MyDumpSender(const char *dump_path,
 
   struct curl_httppost *formpost = NULL;
   struct curl_httppost *lastptr = NULL;
-  struct curl_slist *headerlist = NULL;
-  static const char buf[] = "Expect:";
+  //struct curl_slist *headerlist = NULL;
+  //static const char buf[] = "Expect:";
 
   curl_global_init(CURL_GLOBAL_ALL);
   path.append(file);
@@ -81,25 +81,28 @@ bool MyDumpSender(const char *dump_path,
   curl = curl_easy_init();
   /* initalize custom header list (stating that Expect: 100-continue is not
      wanted */
-  headerlist = curl_slist_append(headerlist, buf);
+  //headerlist = curl_slist_append(headerlist, buf);
   if (curl) {
     /* what URL that receives this POST */
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
 
-    curl_easy_setopt(curl, CURLOPT_VERBOSE, 5);
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 0);
 
     curl_easy_setopt(curl, CURLOPT_HTTPPOST, formpost);
+    LOGDEBUG("Data present, try to send dump");
     res = curl_easy_perform(curl);
+   std::cout<<"dump sended, result="<<res<<std::endl;;
 
     /* always cleanup */
     curl_easy_cleanup(curl);
-
+    std::cout << "curl cleaned up"<<std::endl;
     /* then cleanup the formpost chain */
-    curl_formfree(formpost);
+//    curl_formfree(formpost);
+    std::cout << "form cleaned up"<<std::endl;
     /* free slist */
-    curl_slist_free_all(headerlist);
+    //curl_slist_free_all(headerlist);
   }
-  LOGDEBUG("sended");
+  std::cout<<"sended"<<std::endl;
 
   return true;
 }
