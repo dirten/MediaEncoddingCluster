e69e1b992cff02bd3ac7759f53ea67f2a65f73df
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2010-11-22 19:30:48 +0000

    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@1990 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ test/org/esb/util/DecimalTest.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   DecimalTest.cpp
+ * Author: HoelscJ
+ *
+ * Created on 22. November 2010, 12:48
+ */
+
+#include <cstdlib>
+#include <math.h>
+#include "org/esb/util/Decimal.h"
+#include "org/esb/util/Log.h"
+using namespace std;
+using namespace org::esb::util;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+  Log::open();
+
+  double d=100000./3.;
+  d*=3;
+  Decimal dec(d);
+  Decimal::MantissaType i;
+  Decimal::MantissaType f;
+  Decimal::ExponentType exp = 0;
+  dec.getIntegralFractionalExponent<Decimal::MantissaType>(i,f,exp,Decimal::ExponentType(dec.getExponent()));
+  std::cout<<setprecision(19) <<i<<"-"<<f/pow(10, dec.getExponent()*-1)<<std::endl;
+  std::cout << "digits="<<dec.numberOfDigits<Decimal::MantissaType>(dec.getMantissa())<<std::endl;
+  std::cout << "digits="<<pow(10, dec.getExponent()*-1)<<std::endl;
+//  LOGDEBUG();
+  return 0;
+}
+
