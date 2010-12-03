305f10da249b2dd56d753d1128564d31da27432b
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2010-12-03 20:17:00 +0000

    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@2004 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ src/org/esb/grid/GridNode.cpp
@@ -0,0 +1,71 @@
+/* 
+ * File:   GridNode.cpp
+ * Author: HoelscJ
+ * 
+ * Created on 3. Dezember 2010, 15:30
+ */
+
+#include "GridNode.h"
+#include "boost/date_time/posix_time/posix_time.hpp"
+namespace org {
+  namespace esb {
+    namespace grid {
+
+      GridNode::GridNode() {
+        _name = "null";
+        _status = NODE_UP;
+        _last_activity = boost::posix_time::second_clock::local_time();
+      }
+
+      GridNode::GridNode(boost::asio::ip::udp::endpoint & ep, std::string data) {
+        _ep = ep;
+        _ipaddress = ep.address();
+        _name = "null";
+        _last_activity = boost::posix_time::second_clock::local_time();
+        _status = NODE_UP;
+      }
+      
+      std::string GridNode::toString() {
+        std::ostringstream oss;
+        oss << _ep.address() << ":";
+        oss << _ep.port();
+        //        oss<<":"<<_ep.protocol();
+        oss << ":" << boost::posix_time::to_simple_string(_last_activity);
+        return oss.str();
+      }
+        GridNode::NodeStatus GridNode::getStatus(){
+          return _status;
+        }
+
+      std::string GridNode::getData(std::string key) {
+        return _node_data[key];
+      }
+
+      void GridNode::setData(std::string key, std::string value) {
+        _node_data[key] = value;
+      }
+
+      void GridNode::setEndpoint(boost::asio::ip::udp::endpoint ep) {
+        _ep = ep;
+        //        _ipaddress = ep.address();
+      }
+
+      const boost::asio::ip::address GridNode::getIpAddress()const {
+        return _ep.address();
+      }
+
+      bool GridNode::operator==(const GridNode&a)const {
+        //        logdebug("&operator==");
+        return _ep == a._ep;
+      }
+
+      bool GridNode::operator==(const GridNode*a)const {
+        //        logdebug("*operator==");
+        return _ep == a->_ep;
+      }
+
+      GridNode::~GridNode() {
+      }
+    }
+  }
+}
---
+++ src/org/esb/grid/GridNode.h
@@ -0,0 +1,60 @@
+/* 
+ * File:   GridNode.h
+ * Author: HoelscJ
+ *
+ * Created on 3. Dezember 2010, 15:30
+ */
+
+#ifndef GRIDNODE_H
+#define	GRIDNODE_H
+#include <boost/asio.hpp>
+#include <boost/serialization/binary_object.hpp>
+#include <boost/serialization/map.hpp>
+namespace org {
+  namespace esb {
+    namespace grid {
+
+      class GridNode {
+      public:
+
+        enum NodeStatus {
+          NODE_UP,
+          NODE_DOWN
+        };
+
+        GridNode();
+        virtual ~GridNode();
+        GridNode(boost::asio::ip::udp::endpoint & ep, std::string data);
+        const boost::asio::ip::address getIpAddress()const;
+        const boost::asio::ip::address getName()const;
+        boost::asio::ip::udp::endpoint _ep;
+        bool operator==(const GridNode & a)const;
+        bool operator==(const GridNode * a)const;
+        std::string toString();
+
+        NodeStatus getStatus();
+        std::string getData(std::string key);
+        void setData(std::string key, std::string value);
+        void setEndpoint(boost::asio::ip::udp::endpoint);
+      private:
+        boost::asio::ip::address _ipaddress;
+        std::string _name;
+        NodeStatus _status;
+        std::map<std::string, std::string> _node_data;
+        friend class NodeResolver;
+        boost::posix_time::ptime _last_activity;
+      public:
+
+        template<class Archive>
+        void serialize(Archive & ar, const unsigned int version) {
+          ar & _node_data;
+        }
+
+      };
+
+    }
+  }
+}
+
+#endif	/* GRIDNODE_H */
+
