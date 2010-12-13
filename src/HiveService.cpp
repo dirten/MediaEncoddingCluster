9e937674f55e4a9543fad1d785291da22acad12d
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2010-12-13 22:25:41 +0000

    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@2024 7add9c9f-332d-0410-adcc-f7b1da7091d4

--- src/HiveService.cpp
+++ src/HiveService.cpp
@@ -44,7 +44,14 @@ public:
   void onEvent(ProcessEvent&ev){
     LOGDEBUG("ProcessEvent received");
     if(ev.getEventType()==ProcessEvent::PROCESS_STOPPED){
-      ::kill(getpid(),15);
+#ifdef __WIN32__
+        HANDLE hProcess;
+        hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, GetCurrentProcessId());
+        TerminateProcess(hProcess, (DWORD) - 1);
+        CloseHandle(hProcess);
+#else
+        ::kill(getpid(), 15);
+#endif
     }
   }
 };
