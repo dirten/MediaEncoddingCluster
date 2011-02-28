ab74c4ea482ca4a6cc34607e0e60e1f9a4035c4f
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2011-02-28 18:50:55 +0000

    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@2119 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ test/org/esb/hive/ProjectXmlTest.cpp
@@ -0,0 +1,45 @@
+/* 
+ * File:   ProjectXmlTest.cpp
+ * Author: HoelscJ
+ *
+ * Created on 28. Februar 2011, 13:12
+ */
+
+#include <cstdlib>
+#include "org/esb/util/Log.h"
+#include "org/esb/db/hivedb.hpp"
+#include "org/esb/hive/xml/Project.h"
+#include "org/esb/hive/DatabaseService.h"
+#include "org/esb/io/File.h"
+#include "org/esb/io/FileInputStream.h"
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+  Log::open();
+  org::esb::hive::DatabaseService::createDatabase();
+  org::esb::hive::DatabaseService::updateTables();
+  db::HiveDb db = org::esb::hive::DatabaseService::getDatabase();
+  db::Project prj(db);
+  prj.update();
+  org::esb::hive::Project project(prj);
+
+  org::esb::io::File file(std::string(MEC_SOURCE_DIR) + "/test/testproject.xml");
+  if (!file.exists()) {
+    LOGERROR("Preset File does not exist! " << file.getPath());
+    return -1;
+  }
+  //LOGINFO("reading presets from "<<filename);
+  org::esb::io::FileInputStream fis(&file);
+  std::string data;
+  fis.read(data);
+  project.setXml(data);
+
+  Log::close();
+
+  return 0;
+}
+
