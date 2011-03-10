2eb6692f81411508d4c8b608dbbd3e6836dee165
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2011-03-10 19:33:05 +0000

    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@2144 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ src/org/esb/av/TestFFMpegUrl.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   TestFFMpegUrl.h
+ * Author: HoelscJ
+ *
+ * Created on 9. März 2011, 11:17
+ */
+
+#ifndef TESTFFMPEGURL_H
+#define	TESTFFMPEGURL_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TESTFFMPEGURL_H */
+
