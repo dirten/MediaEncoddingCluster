8bd263a494a73047581ebba4ae0923c0ccb896d5
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2011-03-10 22:03:22 +0000

    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@2147 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ src/org/esb/av/TestFFMpegUrl.cpp
@@ -0,0 +1,36 @@
+#include "AV.h"
+//#include "org/esb/util/Log.h"
+static int test_open(URLContext *h, const char *uri, int flags){
+  printf("test_open\n");
+  return 0;
+}
+
+static int test_read(URLContext *h, uint8_t *buf, int size){
+  printf("test_read\n");
+  return 0;
+}
+
+static int test_write(URLContext *h, const unsigned char *buf, int size){
+  printf("test_write\n");
+  return 0;
+}
+
+static int64_t test_seek(URLContext *h, int64_t pos, int whence){
+  printf("test_seek\n");
+  return 0;
+}
+
+static int test_close(URLContext *h){
+  printf("test_close\n");
+  return 0;
+}
+
+URLProtocol test_protocol = {
+  "test",
+  test_open,
+  test_read,
+  test_write,
+  test_seek,
+  test_close,
+};
+
