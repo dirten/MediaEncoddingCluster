4a84a3d88e8ebd19bd6b02824b4bf43df43a9a94
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2011-04-11 17:28:30 +0000

    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@2168 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ src/org/esb/rpc/services/ApiServiceImpl.cpp
@@ -0,0 +1,70 @@
+/* 
+ * File:   ApiService.cpp
+ * Author: HoelscJ
+ * 
+ * Created on 4. April 2011, 15:05
+ */
+
+#include "ApiServiceImpl.h"
+namespace org {
+  namespace esb {
+    namespace rpc {
+
+      ApiServiceImpl::ApiServiceImpl(Server * s) : _db(org::esb::hive::DatabaseService::getDatabase()) {
+
+      }
+
+      void ApiServiceImpl::authenticate(::google::protobuf::RpcController* controller,
+              const ::org::esb::rpc::User* request,
+              ::org::esb::rpc::AuthToken* response,
+              ::google::protobuf::Closure* done){
+        _authtoken="bla";
+        response->set_token(_authtoken);
+      }
+
+      void ApiServiceImpl::getMediafile(::google::protobuf::RpcController* controller,
+              const ::org::esb::rpc::MediafileRequest* request,
+              ::org::esb::rpc::MediafileResponse* response,
+              ::google::protobuf::Closure* done) {
+
+      }
+
+      void ApiServiceImpl::addMediafile(::google::protobuf::RpcController* controller,
+              const ::org::esb::rpc::MediafileRequest* request,
+              ::org::esb::rpc::MediafileResponse* response,
+              ::google::protobuf::Closure* done) {
+
+      }
+
+      void ApiServiceImpl::getMediafileList(::google::protobuf::RpcController* controller,
+              const ::org::esb::rpc::MediafileRequest* request,
+              ::org::esb::rpc::MediafileResponse* response,
+              ::google::protobuf::Closure* done) {
+        if(_authtoken.length()==0){
+          controller->SetFailed("not authenticated");
+          return;
+        }else{
+          LOGDEBUG("Token="<<_authtoken);
+        }
+
+        LOGDEBUG(boost::this_thread::get_id());
+        vector<db::MediaFile>files = litesql::select<db::MediaFile > (_db).all();
+        vector<db::MediaFile>::iterator it = files.begin();
+        for (; it != files.end(); it++) {
+          Mediafile * file = response->mutable_mediafiles()->Add();
+          file->set_id((*it).id);
+          file->set_filename((*it).filename);
+          file->set_size((*it).filesize);
+          file->set_duration((*it).duration);
+          file->set_format((*it).containertype);
+          file->set_stream_count((*it).streamcount);
+        }
+        response->set_status(0);
+      }
+
+      ApiServiceImpl::~ApiServiceImpl() {
+      }
+
+    }
+  }
+}
---
+++ src/org/esb/rpc/services/ApiServiceImpl.h
@@ -0,0 +1,50 @@
+/* 
+ * File:   ApiService.h
+ * Author: HoelscJ
+ *
+ * Created on 4. April 2011, 15:05
+ */
+
+#ifndef APISERVICE_H
+#define	APISERVICE_H
+#include "org/esb/rpc/rpc.pb.h"
+#include "org/esb/util/Log.h"
+#include "org/esb/hive/DatabaseService.h"
+
+namespace org {
+  namespace esb {
+    namespace rpc {
+      class Server;
+
+      class ApiServiceImpl : public org::esb::rpc::ApiService {
+        classlogger("org.esb.rpc.ApiService");
+      public:
+        ApiServiceImpl(Server*);
+        virtual ~ApiServiceImpl();
+        void authenticate(::google::protobuf::RpcController* controller,
+                const ::org::esb::rpc::User* request,
+                ::org::esb::rpc::AuthToken* response,
+                ::google::protobuf::Closure* done);
+
+        void getMediafile(::google::protobuf::RpcController* controller,
+                const ::org::esb::rpc::MediafileRequest* request,
+                ::org::esb::rpc::MediafileResponse* response,
+                ::google::protobuf::Closure* done);
+        void addMediafile(::google::protobuf::RpcController* controller,
+                const ::org::esb::rpc::MediafileRequest* request,
+                ::org::esb::rpc::MediafileResponse* response,
+                ::google::protobuf::Closure* done);
+        void getMediafileList(::google::protobuf::RpcController* controller,
+                const ::org::esb::rpc::MediafileRequest* request,
+                ::org::esb::rpc::MediafileResponse* response,
+                ::google::protobuf::Closure* done);
+
+      private:
+        db::HiveDb _db;
+        std::string _authtoken;
+      };
+    }
+  }
+}
+#endif	/* APISERVICE_H */
+
