bd7c6fe4406af4dd0eb631f49f60a60d6090c9e3
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2011-04-23 23:01:28 +0000

    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@2197 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ src/org/esb/io/exports.h
@@ -0,0 +1,12 @@
+#ifndef ORG_ESB_IO_EXPORTS
+#define ORG_ESB_IO_EXPORTS
+  #if defined(_WIN32)
+    #if defined(io_EXPORTS)
+      #define IO_EXPORT __declspec( dllexport )
+    #else
+      #define IO_EXPORT __declspec( dllimport )
+    #endif
+  #else
+    #define IO_EXPORT
+  #endif
+#endif
