6a297eeab8c85282b938acc5affad85dadbac882
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2011-08-20 16:32:37 +0000

    adding util/foreach.h
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@2370 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ src/org/esb/util/Foreach.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   Foreach.h
+ * Author: HoelscJ
+ *
+ * Created on 19. August 2011, 14:09
+ */
+
+#ifndef FOREACH_H
+#define	FOREACH_H
+#include <boost/foreach.hpp>
+#define foreach BOOST_FOREACH
+
+#endif	/* FOREACH_H */
+
