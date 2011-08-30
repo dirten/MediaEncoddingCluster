e57233b15752269344d1ccb2589c37b06b2ba224
framebuster <framebuster@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2011-08-30 18:40:56 +0000

    - implementing new plugin structure
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@2385 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ test/org/esb/api/ApiWebserverTest.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   ApiWebserverTest.cpp
+ * Author: HoelscJ
+ *
+ * Created on 30. August 2011, 13:04
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+  return 0;
+}
+
