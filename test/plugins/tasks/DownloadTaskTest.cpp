9bef1bc277be59c60c3ecfc28fabc4b00f54eb3b
jhoelscher <jhoelscher@7add9c9f-332d-0410-adcc-f7b1da7091d4>
2011-10-19 15:43:48 +0000

    - added DownloadTaskTest
    git-svn-id: http://svn.codergrid.de/svn/sf/bruteripper/trunk@2567 7add9c9f-332d-0410-adcc-f7b1da7091d4

---
+++ test/plugins/tasks/DownloadTaskTest.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   DownloadTaskTest.cpp
+ * Author: HoelscJ
+ *
+ * Created on 19. Oktober 2011, 14:03
+ */
+
+#include <cstdlib>
+#include "org/esb/core/PluginRegistry.h"
+#include "org/esb/core/Task.h"
+
+#include "org/esb/util/Log.h"
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+  Log::open();
+  org::esb::core::PluginRegistry::getInstance()->load(DOWNLOADTASK_PLUGIN);
+  org::esb::core::PluginRegistry::getInstance()->initPlugins();
+  std::string cfg = "downloadtask.src=http://www.google.de;downloadtask.trg=/tmp/index.html";
+  {
+    Ptr<org::esb::core::Task> task = org::esb::core::PluginRegistry::getInstance()->createTask("DownloadTask", cfg);
+    if (task) {
+      task->prepare();
+      task->execute();
+    }
+  }
+  org::esb::core::PluginRegistry::close();
+  return 0;
+}
+
