fe160444b0cc023266f795b8a627037e4ecdfb5f
Jan Hölscher <jhoelscher@jan-holschers-macbook.fritz.box>
2013-09-29 19:21:45 +0200

    - cosmetic

--- web/app/view/flow/List.js
+++ web/app/view/flow/List.js
@@ -1,38 +1,38 @@
 Ext.define('MEC.view.flow.List' ,{
-    extend: 'Ext.grid.Panel',
-    alias: 'widget.flowlist',
+               extend: 'Ext.grid.Panel',
+               alias: 'widget.flowlist',
 
-    title: 'All Flows',
-    store : 'Flows',
-    hideHeaders:true,
-    initComponent: function() {
+               title: 'All Flows',
+               store : 'Flows',
+               hideHeaders:true,
+               initComponent: function() {
 
-        this.columns = [
-            {header: 'Name', dataIndex: 'name', flex: 1}
-        ];
+                   this.columns = [
+                            {header: 'Name', dataIndex: 'name', flex: 1}
+                        ];
 
-	this.dockedItems = [{
-            dock: 'bottom',
-            xtype: 'toolbar',
-            items: [{
-                xtype: 'button',
-                text: 'Settings',
-                action: 'settings'
-            }, {
-                xtype: 'buttongroup',
-                items: [{
-                xtype: 'button',
-                    text: 'By Date',
-                    action: 'filter-date'
-                }, {
-                xtype: 'button',
-                    text: 'ABC',
-                    action: 'filter-name'
-                }]
-            }]
-        }];
-        this.callParent(arguments);
-    
-    }
+                   this.dockedItems = [{
+                                           dock: 'bottom',
+                                           xtype: 'toolbar',
+                                           items: [{
+                                                   xtype: 'button',
+                                                   text: 'Settings',
+                                                   action: 'settings'
+                                               }, {
+                                                   xtype: 'buttongroup',
+                                                   items: [{
+                                                           xtype: 'button',
+                                                           text: 'By Date',
+                                                           action: 'filter-date'
+                                                       }, {
+                                                           xtype: 'button',
+                                                           text: 'ABC',
+                                                           action: 'filter-name'
+                                                       }]
+                                               }]
+                                       }];
+                   this.callParent(arguments);
 
-});
+               }
+
+           });
