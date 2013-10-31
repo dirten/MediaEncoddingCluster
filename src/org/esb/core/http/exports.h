f5751da62f4e9e4520f7e6f5a890d67aecd7e931
Jan Hoelscher <jh@codergrid.de>
2013-10-31 17:54:07 +0100

    - windows compile issues fixed

--- src/org/esb/core/http/exports.h
+++ src/org/esb/core/http/exports.h
@@ -1,7 +1,7 @@
 #ifndef ORG_ESB_CORE_HTTP_EXPORTS
 #define ORG_ESB_CORE_HTTP_EXPORTS
   #if defined(_WIN32) 
-    #if defined(core_http_EXPORTS)
+    #if defined(core_EXPORTS)
       #define CORE_HTTP_EXPORT __declspec( dllexport )
     #else
       #define CORE_HTTP_EXPORT __declspec( dllimport )
