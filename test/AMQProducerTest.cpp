029b7475552f5667e98ae21cd680fd180021d3ca
Jan Hölscher <jhoelscher@janholsmacbook2.wkd.wolterskluwer.de>
2013-11-21 11:12:52 +0100

    - test active mq

--- test/AMQProducerTest.cpp
+++ test/AMQProducerTest.cpp
@@ -17,19 +17,21 @@ int main(int argc, char ** argv){
 
   Session*session = connection->createSession(Session::SESSION_TRANSACTED);
   //Session*session = connection->createSession(Session::AUTO_ACKNOWLEDGE);
-  Destination* destination = session->createQueue("TEST.FOO");
+  Destination* destination = session->createQueue("MHIVE.OUTPUT");
   MessageProducer*producer = session->createProducer(destination);
   producer->setDeliveryMode(DeliveryMode::PERSISTENT);
 
-
+  int l=1*1024*1024;
+  unsigned char * bytes=new unsigned char[l];
   for (int ix = 0; ix < 1000; ++ix) {
-    TextMessage* message(session->createTextMessage("neue nachricht"));
+    //TextMessage* message(session->createTextMessage("neue nachricht"));
+    BytesMessage* message(session->createBytesMessage(bytes,l));
     message->setIntProperty("Integer", ix);
     printf("Sent message #%d from thread %s\n", ix + 1, "bla");
     producer->send(message);
     delete message;
+    session->commit();
   }
-  session->commit();
   connection->close();
   return 0;
 }
