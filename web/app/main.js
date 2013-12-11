c9b12fe22393e82ad6e332fda782148381a3d650
Jan Hoelscher <jh@codergrid.de>
2013-12-11 18:56:56 +0100

    -  web development

---
+++ web/app/main.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function () {
+                      $( "#date" ).datepicker();
+
+
+                      $('#tabs')
+                          .tabs()
+                          .addClass('ui-tabs-vertical ui-helper-clearfix');
+                  });
