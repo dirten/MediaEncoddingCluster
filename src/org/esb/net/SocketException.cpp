9c27b76cf88da2a07a6b20fdacf3bde32944f21e
Jan Hölscher <jhoelscher@janholsmacbook2.wkd.wolterskluwer.de>
2014-03-03 16:42:55 +0100

    - windows compile the libraries in shared environment

--- src/org/esb/net/SocketException.cpp
+++ src/org/esb/net/SocketException.cpp
@@ -1,16 +1,16 @@
 #include <string.h>
 #include "SocketException.h"
-#include "org/esb/util/Log.h"
+
 
 
 
 using namespace org::esb::net;
 
 SocketException::SocketException(std::string & msg):org::esb::lang::Exception(msg){
-	LOGERROR(msg.c_str());
+  //LOGERROR(msg.c_str());
 }
 SocketException::SocketException(const char * msg):org::esb::lang::Exception(msg){
-	LOGERROR(msg);
+  //LOGERROR(msg);
 }
 
 
