c6dffca02355ad77ec6a24540fd5620751b54e2c
Jan Hoelscher <jh@codergrid.de>
2014-03-03 11:32:08 +0100

    - cleanup the library chaos

---
+++ src/org/esb/global/GlobalLibrary.cpp
@@ -0,0 +1,5 @@
+#include "GlobalLibrary.h"
+
+GlobalLibrary::GlobalLibrary()
+{
+}
---
+++ src/org/esb/global/GlobalLibrary.h
@@ -0,0 +1,10 @@
+#ifndef GLOBALLIBRARY_H
+#define GLOBALLIBRARY_H
+
+class GlobalLibrary
+{
+  public:
+    GlobalLibrary();
+};
+
+#endif // GLOBALLIBRARY_H
